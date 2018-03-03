d2b4e9d9d1200693b6189331b5dc1a673061e917
renrizzolo <renrizmusic@gmail.com>
2018-03-03 16:29:42 +1100

    move all releases to json files

--- src/views/Music/index.js
+++ src/views/Music/index.js
@@ -1,90 +1,14 @@
 import React from 'react';
 import { Fade } from '../../animations/';
 import releases from './releases.json';
+import freebies from './freebies.json';
+import mixes from './mixes.json';
 import {
   Route,
   Link
 } from 'react-router-dom';
-import '../../../styles/common/item-grid.scss';
+import '../../styles/common/item-grid.scss';
 
-
-
-//these could be json files that can be edited directly on gitHub
-// const releases = [
-// 	{
-// 		name: 'Every Time EP',
-// 		artist: 'Ren Riz',
-// 		id:'CSM001',
-// 		imgSource: './img/covers/every-time-ep.jpg',
-// 		previewHTML: '<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/44054952&amp;color=%23168dec&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false&amp;visual=true"></iframe>'
-// 		,links: [
-// 			{
-// 				name: 'iTunes',
-// 				url: 'https://itunes.apple.com/au/album/every-time-ep/id892096132',
-// 			},
-// 			{
-// 				name: 'Spotify',
-// 				url: 'https://open.spotify.com/album/2UlNv3QKvo5xSHwCxtiuPj',
-// 			},
-// 			{
-// 				name: 'beatport',
-// 				url: 'https://www.beatport.com/release/every-time-ep/1331503',
-// 			}
-// 		]
-// 	},
-// ];
-
-const mixes = [
-	{
-		'name': 'Casual Sparks vinyl mix vol. II',
-		'artist': 'Ren Riz',
-		'previewHTML': '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/158611186&amp;color=%230066cc&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false"></iframe>',
-		'trackList': [
-			'Leon Vynehall - Inside The Deku Tree [3024]',
-			'Creative Swing Alliance - In My Arms [Wolf Music]',
-			'Gerome Sportelli - Free Range [Sporty Limited]',
-			'Glenn Astro & IMYRMiND - Got Me Shakin\' [Outernational]',
-			'Max Graef & Labuzinski - Drums of Death [Tartelet]',
-			'Iron Curtis - Love Commitment [Retreat]',
-			'Juice Belushi - How Do You See? [Gator Boots]',
-			'Fantastic Man - Sanctuary [Let’s Play House]',
-			'VIBES Ltd.VoL 3 - Untitled A1 [Vibes Ltd]',
-			'Detroit Swindle - Woman [Free Range]',
-			'André Laos - Yield [Let’s Play House]',
-			'Greymatter - Night Soldier ft. Sophie Brown [Wolf Music]',
-			'Francis Inferno Orchestra - Vibrations [Let’s Play House]',
-			'HNNY - Always (Short Edit) [Omena]',
-			'E.S.P - It’s You (vocal) [Underground]',
-			'Mr Fingers - Can You Feel It (original instrumental mix) [Trax]',
-			'Psychotropic - Get Your Thing Together [02]',
-		]
-	},
-	{
-		'name': 'Casual Sparks vinyl mix vol. I',
-		'artist': 'Ren Riz',
-		'previewHTML': '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/137472879&amp;color=%230066cc&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false"></iframe>',
-
-	}
-];
-
-const freebies = [
-	{
-		'name': 'Marmalade',
-		'artist': 'Ren Riz',
-		//'imgSource': 'marmalade.jpg',
-		'previewHTML': '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/226389239&amp;color=%230066cc&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false"></iframe>',				
-	},
-	{
-		'name': 'Through With You',
-		"artist": "Ren Riz",
-		"previewHTML": "<iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/107452559&amp;color=%23168dec&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false'></iframe>",
-	},
-	{
-		'name': 'Call Me Up (ft. Vincent Coleman)',
-		"artist": "Ren Riz",
-		"previewHTML": "<iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/181096805&amp;color=%23168dec&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=false'></iframe>",
-	}
-];
 function expandList(e) {
 	e.target.parentElement.classList.toggle('expanded');
 }
